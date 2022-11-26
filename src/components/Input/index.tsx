18bf3139dcf412e61071dc22edccf577ec9a3720
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-11-26 18:27:49 -0300

    feat: Style input and form

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './style'
+
+const InputForm = () => {
+  return (
+    <S.ContainerInput>
+      <S.Input placeholder="Digite o IP" />
+    </S.ContainerInput>
+  )
+}
+
+export default InputForm
