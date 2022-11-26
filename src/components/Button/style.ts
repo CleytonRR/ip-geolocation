51c1d952644b2fa6bbc7381fd249fd5d176ed136
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-11-26 18:19:46 -0300

    feat: Added style in button

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,3 +1,21 @@
 import styled from 'styled-components'
 
-export const Button = styled.button``
+export const Button = styled.button`
+  outline: none;
+  background-color: var(--yellow);
+  color: var(--black);
+  border-radius: 14px;
+  margin-top: 64px;
+  width: 235px;
+  height: 64px;
+  border: none;
+  font-size: 2.4rem;
+  font-weight: bold;
+  cursor: pointer;
+  box-shadow: 0px 3px 8px rgba(28, 27, 45, 0.75);
+  transition: all 200ms ease-in;
+
+  &:hover {
+    background-color: var(--green);
+  }
+`
