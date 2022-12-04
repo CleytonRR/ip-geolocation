9d0a2eb1ad334558b26d424fd58b5c7209fa4d9e
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-12-04 20:16:58 -0300

    Icon map (#3) :fire:
    * assets: Added icons
    
    * feat: Added styles to icons
    
    * feat: Added struct to icons
    
    * feat: Added icons section in page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Hero from 'components/Hero'
+import InfoLocation from 'components/Info-Location'
 import InputForm from 'components/Input'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Hero />
       <InputForm />
+      <InfoLocation />
     </>
   )
 }
