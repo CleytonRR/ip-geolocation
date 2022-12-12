5704c7b2fde6d8b66cd864a11c4cc578969cf8d0
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-12-11 21:48:27 -0300

    feat: Added function to get ip data

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
 import Button from 'components/Button'
 import { useState } from 'react'
+import client from 'utils/api'
 import maskIp from './formtIp'
 import * as S from './style'
 
@@ -13,13 +14,21 @@ const InputForm = () => {
     setIp(formatIp)
   }
 
+  async function submitForm(e: React.ChangeEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const ipLocationInfo = await client.get(`/${ip}`)
+
+    console.log(ipLocationInfo.data)
+  }
+
   return (
-    <S.ContainerInput>
+    <S.ContainerInput onSubmit={submitForm}>
       <S.Input
         value={ip}
         onChange={handleIp}
         placeholder="Digite o IP"
         maxLength={15}
+        required
       />
       <Button small>Buscar</Button>
     </S.ContainerInput>
