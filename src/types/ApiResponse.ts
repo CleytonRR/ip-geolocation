b85654259878bc115b9fac1fa8facc39e06b2807
Cleyton Furtado <cleytonrodrigues69@live.com>
2022-12-11 22:55:34 -0300

    feat: added update function type

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,17 +1,21 @@
 export interface ApiResponse {
-  query: string
-  message: string
-  status: string
-  country: string
-  countryCode: string
-  region: string
-  regionName: string
-  city: string
-  zip: string
-  lat: number
-  lon: number
-  timezone: string
-  isp: string
-  org: string
-  as: string
+  query?: string
+  message?: string
+  status?: string
+  country?: string
+  countryCode?: string
+  region?: string
+  regionName?: string
+  city?: string
+  zip?: string
+  lat?: number
+  lon?: number
+  timezone?: string
+  isp?: string
+  org?: string
+  as?: string
+}
+
+export interface UpdateApiResponseData {
+  setApiResponse?(apiResponse: ApiResponse): void
 }
